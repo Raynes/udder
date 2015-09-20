c6336b6bb45293d5e917594ee783ef5f685ff01b
Anthony Grimes <i@raynes.me>
2015-09-19 22:48:34 -0700

    Fix some indentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var jsdoc = require("gulp-jsdoc");
 
-gulp.task("babel", function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('lib'));
+gulp.task("babel", function() {
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
 });
 
 gulp.task("watch", function() {
