c1c6cbd83b3a40dfa68d69ef6f537fa6430812a6
Anthony Grimes <i@raynes.me>
2015-09-20 17:53:37 -0700

    Add support for numeric ranges

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,5 @@
+import {toSpoken} from "spoken-numbers";
+
 export default function parseUtterance(utterance) {
   let output = [];
 
@@ -59,7 +61,7 @@ function parseText(utterance, position) {
 
   for (var i = position; i < utterance.length; ++i) {
     // check for any special cut chars
-    if (['{', '}', ',', '|'].indexOf(utterance[i]) !== -1) {
+    if (['{', '}', ',', '|', '…'].indexOf(utterance[i]) !== -1) {
       return [text, i];
     }
 
@@ -69,30 +71,66 @@ function parseText(utterance, position) {
   return [text, i];
 }
 
+function parseRange(utterance, leftSide, pos) {
+  let rightSide;
+  let choices = [];
+
+  if (!leftSide || !leftSide.length) {
+    leftSide = 0;
+  } else {
+    leftSide = Number.parseInt(leftSide);
+    if (!(leftSide || leftSide >= 0)) {
+      syntaxError(utterance, pos, "not a number");
+    }
+  }
+
+  [rightSide, pos] = parseText(utterance, pos);
+
+  if (!rightSide.length) {
+    syntaxError(utterance, pos, "end of range required");
+  } else {
+    rightSide = Number.parseInt(rightSide);
+    if (!rightSide) {
+      syntaxError(utterance, pos, "not a number");
+    } else {
+      for (var start = leftSide; start <= rightSide; start++) {
+        choices.push(toSpoken(start));
+      }
+
+      return [choices, pos - 1];
+    }
+  }
+}
+
 function parseSlot(utterance, position) {
   let slotChoices = [];
   let slotType;
 
   for(var i = position; i < utterance.length; ++i) {
     let [text, pos] = parseText(utterance, i);
-    slotChoices.push(text);
+    if (text && text.length > 0) slotChoices.push(text);
     i = pos;
 
     switch (utterance[pos]) {
-    case ',': break;
-    case '}':
-      return [{type: "slot", choices: slotChoices, slotType: slotType}, i];
-    case '|':
-      [slotType, pos] = parseText(utterance, i + 1);
+      case ',': break;
+      case '}':
+        return [{type: "slot", choices: slotChoices, slotType: slotType}, i];
+      case '…':
+        let choices;
+        [choices, i] = parseRange(utterance, slotChoices.pop(), i + 1);
+        slotChoices = slotChoices.concat(choices);
+        break;
+      case '|':
+        [slotType, pos] = parseText(utterance, i + 1);
 
-      if (utterance[pos] !== '}') {
-        syntaxError(utterance, pos, 'no closing brace for slot');
+        if (utterance[pos] !== '}') {
+          syntaxError(utterance, pos, 'no closing brace for slot');
+        }
+
+        return [{type: "slot", choices: slotChoices, slotType: slotType}, pos];
+      default:
+        syntaxError(utterance, pos, 'unexpected input');
       }
-
-      return [{type: "slot", choices: slotChoices, slotType: slotType}, pos];
-    default:
-      syntaxError(utterance, pos, 'unexpected input');
-    }
   }
 
   syntaxError(utterance, position, "no closing brace for slot");
